c9dae81cd7f5b000dcac18565e99748934ff8f3c
DanSev7 <danielayele077@gmail.com>
2025-12-15 22:34:28 +0300

    latest

--- config/environment.ts
+++ config/environment.ts
@@ -1,4 +1,6 @@
 // Environment configuration
+// NOTE: During development, replace '192.168.1.6' with your actual machine IP address
+// You can find your IP by running 'ipconfig' on Windows or 'ifconfig' on Mac/Linux
 const ENV = {
   development: {
     apiUrl: 'http://192.168.1.6:5000/api', // Your actual IP address
